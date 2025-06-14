d0a2ea6037eb6c2a686810d8a13ed6571d3d01c1
Jayedjb765 <jayedjb76@gmail.com>
2025-06-14 22:11:13 +0600

    adding rtk query

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,11 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
-import taskReducer from "./features/task/taskSlice";
-import userReducer from "./features/user/userSlice";
+
 export const store = configureStore({
-  reducer: {
-    todo: taskReducer,
-    alluser: userReducer,
-  },
+  reducer: {},
 });
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>;
